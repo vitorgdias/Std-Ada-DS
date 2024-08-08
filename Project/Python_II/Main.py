edeaee59fe839eb942ef914069c78ddc603332a1
vitorgdias <vitorgalves@live.com>
2024-08-08 12:06:38 -0300

    Função Sistema atualizada.

--- Project/Python_II/Main.py
+++ Project/Python_II/Main.py
@@ -1,10 +1,18 @@
+from Product import Create_Register
+
 def active_system():
-    print('\nBem-vindo(a) ao sistema "Nome do Sistema". Selecione uma das opções abaixo:')
-    print('1. Criar Registro.')
-    print('2. Ler Registro.')
-    print('3. Atualizar Registro.')
-    print('4. Deletar Registro.')
-    return
+    while True:
+        print('\nBem-vindo(a) ao sistema "Nome do Sistema".')
+        print('1. Criar Registro')
+        print('2. Ler Registro')
+        print('3. Atualizar Registro')
+        print('4. Deletar Registro')
+        print('5. Sair')
+        option = input('Selecione uma das opções acima de 1 à 5: ')
+        if  option == '1':
+            Create_Register()
+        else:
+            break
 
 if __name__ == '__main__':
     active_system()
