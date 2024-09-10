b3aab19972e66427b2b8dc1c671a3f93290ea262
Vitor Galves <vitorgalves@live.com>
2024-09-10 10:32:32 -0300

    Delete Project/Techniques/Project/CSV_Reader.py

--- Project/Techniques/Project/CSV_Reader.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-file_path = 'Project\Techniques\Project\mercadoimobiliario.csv'
-
-with open(file_path, mode='r') as file:
-    reader = csv.reader(file)
-    row_count = sum(1 for row in reader)
-
-print(f'Número de linhas: {row_count}')
